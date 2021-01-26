c73d421d3678eb056a922f0886257217cdfed0c6
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-01-27 02:03:33 +0530

    refactor : changed edit and delete icon setting

--- index.js
+++ index.js
@@ -1,3 +1,214 @@
+// const URL = "http://localhost:2000/greetings";
+
+// /**
+//  * @description: validate the name from form
+//  * @returns: true or false
+//  * @param:{string}name
+//  */
+// function validFormInputs(name) {
+//     return /^[A-Z]{1}[A-za-z]{2,}$/.test(name);
+// }
+
+// /**
+//  *  function to fetch all greetings
+//  */
+// getData = () => {
+//     fetch(URL)
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw Error("ERROR");
+//             }
+//             return response.json();
+//         })
+//         .then((data) => {
+//             console.log(data.data);
+//             const html = data.data
+//                 .map((greeting) => {
+//                     return `
+//           <div class="user">
+//           <p> ${greeting._id}</p>
+//           <p>${greeting.name}</p>
+//           <p>${greeting.message}</p>
+//           <pre>${greeting.createdAt.substring(0, 10)}</pre>   
+//           <div class='editOnCards'><img src="./assets/edit_icon3.png" onclick="editGreetingPopup('${greeting._id}')"></div>
+//           <div class='deleteOnCards'><img src="./assets/delete_icon3.png" onclick="deleteGreetingPopup('${
+//             greeting._id
+//           }')"></div>
+//           </div>`;
+//                 })
+//                 .join("");
+//             document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// };
+
+// //to load the all grettings
+// addEventListener("click", getData());
+
+// //use to display the add form
+// function openForm() {
+//     document.querySelector(".formPopup").style.display = "none";
+//     closeForm();
+// }
+
+// //use to hide the add form
+// function closeForm() {
+//     document.querySelector(".formPopup").style.display = "none";
+// }
+
+
+// /**
+//  * vallidate the user input and calls add function
+//  * @param {*} greeting
+//  */
+// function addGreeting() {
+//     greeting.preventDefault();
+//     let firstName = document.querySelector(".firstName").value;
+//     let lastName = document.querySelector(".lastName").value;
+//     let form = document.querySelector(".formPopup");
+//     let firstNameError = document.querySelector(".firstNameError");
+//     let lastNameError = document.querySelector(".lastNameError");
+
+//     if (!validFormInputs(firstName) || !validFormInputs(lastName)) {
+//         form.addEventListener("submit", (e) => {
+//             firstNameError.innerHTML = " ";
+//             lastNameError.innerHTML = " ";
+//             e.preventDefault();
+//             let firstNameMessage = [];
+//             let lastNameMessage = [];
+//             if (firstName.length < 3) {
+//                 firstNameMessage.push("first name should be greater than 3 char");
+//             }
+//             if (!validFormInputs(firstName)) {
+//                 firstNameMessage.push("first name should not contain number");
+//             }
+//             if (lastName.length < 3) {
+//                 lastNameMessage.push("last name should be greater than 3 char");
+//             }
+//             if (!validFormInputs(lastName)) {
+//                 lastNameMessage.push("last name should not contain number");
+//             }
+//             if (firstNameMessage.length > 0)
+//                 firstNameError.innerHTML = firstNameMessage.join(", ");
+//             if (lastNameMessage.length > 0)
+//                 lastNameError.innerHTML = lastNameMessage.join(", ");
+//         });
+//     } else add(firstName, lastName);
+// }
+
+// /**
+//  * add the greetings
+//  * @param{firstName, lastName}
+//  */
+// add = (firstName, lastName) => {
+//     fetch(URL, {
+//             method: "POST",
+//             headers: {
+//                 Accept: "application/json, text/plain, */*",
+//                 "Content-type": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 name: firstName,
+//                 message: lastName,
+//             }),
+//         })
+//         .then((response) => {
+//             response.json();
+//             alert("Greeting successfully added");
+//             location.reload();
+//         })
+//         .catch((err) => {
+//             return err;
+//         });
+//     closeForm();
+// };
+
+// //use to add the data on submit
+// document.getElementById("addpost").addEventListener("submit", addGreeting);
+
+// deleteGreetingPopup = (id) => {
+//     document.querySelector(".delete-form-popup").style.display = "flex";
+//     document.getElementById("deleteRecord").addEventListener("click", () => {
+//         deleteGreeting(id);
+//     });
+// };
+
+// document.getElementById("closeDeleteGreetingForm").addEventListener('click', () => {
+//     document.querySelector('.delete-form-popup').style.display = 'none';
+// })
+
+// deleteGreeting = (greetingId) => {
+//     let id = greetingId;
+//     url = `${URL}/${id}`;
+//     fetch(url, { method: "DELETE" })
+//         .then(() => {
+//             location.reload();
+//             alert("Greeting deleted Successfully");
+//         }).catch((err) => {
+//             console.log(err);
+//             alert("Error occcured while deleting greeting try again..!!");
+//         })
+// }
+
+// document.getElementById("closeUpdateGreetingForm").addEventListener('click', () => {
+//     document.querySelector('.update-form-popup').style.display = 'none';
+// })
+
+// editGreetingPopup = (greetingId) => {
+//     document.querySelector('.update-form-popup').style.display = 'flex';
+//     console.log(document.getElementById("updateRecord"));
+//     document.getElementById("updateRecord").addEventListener('click', () => {
+//         editGreeting(greetingId);
+//     });
+// }
+
+// editGreeting = (greetingId) => {
+//     document.getElementById('updateGreeting').addEventListener('submit', (e) => {
+//         var name = document.getElementById('updateName').value;
+//         var message = document.getElementById('updateMsg').value;
+//         let id = greetingId;
+//         url = `${URL}/${id}`;
+//         fetch(url, {
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//             },
+//             method: "PUT",
+//             body: JSON.stringify({
+//                 name: name,
+//                 message: message
+//             }),
+//         }).then((response) => {
+//             console.log(response)
+//             return response.json();
+//         }).then((greetingData) => {
+//             console.log(greetingData);
+//             location.reload();
+//             alert("Greeting updated Successfully");
+//         }).catch((err) => {
+//             console.log(err);
+//             alert("Error occcured while updating greeting try again..!!");
+//         })
+//     })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const URL = "http://localhost:2000/greetings";
 
 
@@ -28,10 +239,11 @@ getData = () => {
                     return `
           <div class="user">
           <p> ${greeting._id}</p>
-          <p>${greeting.name}</p>
-          <p>${greeting.message}</p>
+          <p>${greeting.message} (Greeting)</p>
+          <p>${greeting.name} (name)</p>
           <pre>${greeting.createdAt.substring(0, 10)}</pre>   
           <div class='editOnCards'><img src="./assets/edit_icon3.png" onclick="editGreetingPopup('${greeting._id}')"></div>
+          <br>
           <div class='deleteOnCards'><img src="./assets/delete_icon3.png" onclick="deleteGreetingPopup('${
             greeting._id
           }')"></div>
@@ -133,7 +345,6 @@ deleteGreetingPopup = (id) => {
         deleteGreeting(id);
     });
 };
-
 
 document.getElementById("closeDeleteGreetingForm").addEventListener('click', () => {
     document.querySelector('.delete-form-popup').style.display = 'none';
@@ -280,10 +491,10 @@ editGreeting = (greetingId) => {
                 message: message
             }),
         }).then((response) => {
-            //console.log(response)
+            console.log(response)
             return response.json();
         }).then((greetingData) => {
-            // console.log(greetingData);
+            console.log(greetingData);
             location.reload();
             alert("Greeting updated Successfully");
 
